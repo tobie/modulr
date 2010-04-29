9e2476206b9f6a695e20a4f69b05eeca72a4dd86
Tobie Langel <tobie.langel@gmail.com>
2010-04-30 00:41:10 +0200

    Allow the global export method to handle nested namespaces.

--- lib/modulr/global_export_collector.rb
+++ lib/modulr/global_export_collector.rb
@@ -7,11 +7,26 @@ module Modulr
     end
     
     def to_js(buffer = '')
-      buffer << "var #{@global} = (function() {\n"
+      buffer << "#{define_global} = (function() {\n"
       buffer << File.read(PATH_TO_MODULR_SYNC_JS)
       buffer << transport
       buffer << "\n  return require('#{main.id}');\n"
       buffer << "})();\n"
+    end
+    
+    def define_global
+      if @global.include?('.')
+        props = @global.split('.')
+        str = props.shift
+        results = "var #{str};"
+        props.each do |prop|
+          results << "\n#{str} = #{str} || {};"
+          str << ".#{prop}"
+        end
+        "#{results}\n#{str}"
+      else
+        "var #{@global}"
+      end
     end
   end
 end
