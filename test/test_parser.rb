e79e6ff03e999ddfae321c78627a59f530a0a7da
Tobie Langel <tobie.langel@gmail.com>
2010-05-26 14:38:32 +0200

    Add more parser tests.

--- test/test_parser.rb
+++ test/test_parser.rb
@@ -20,15 +20,29 @@ class TestLibModulrParser < Test::Unit::TestCase
     assert_equal 'foo', get_module_identifier("require('foo').bar;")
   end
   
+  def test_accessing_nested_property
+    assert_equal 'foo', get_module_identifier("require('foo').baz.bar;")
+  end
+  
   def test_calling_method
     assert_equal 'foo', get_module_identifier("require('foo').bar();")
   end
   
+  def test_calling_nested_method
+    assert_equal 'foo', get_module_identifier("require('foo').bar.baz();")
+  end
+  
   def test_instanciating_constructor
     assert_equal 'foo', get_module_identifier("new require('foo').Foo();")
-    assert_equal 'foo', get_module_identifier("new require('foo').foo.Foo();")
-    assert_equal 'foo', get_module_identifier("new (require('foo')).Foo();")
     assert_equal 'foo', get_module_identifier("new require('foo').Foo;")
     assert_equal 'foo', get_module_identifier("new require('foo').Foo(1, 2, 3);")
+    assert_equal 'foo', get_module_identifier("new (require('foo')).Foo();")
+  end
+  
+  def test_instanciating_nested_constructor
+    assert_equal 'foo', get_module_identifier("new require('foo').bar.Foo();")
+    assert_equal 'foo', get_module_identifier("new require('foo').bar.Foo;")
+    assert_equal 'foo', get_module_identifier("new require('foo').bar.Foo(1, 2, 3);")
+    assert_equal 'foo', get_module_identifier("new (require('foo').bar).Foo();")
   end
 end
